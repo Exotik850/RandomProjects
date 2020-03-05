f9ddd808722d125bc8eed3cd12262699a40fec04
Exotik850 <kidkool850@gmail.com>
2020-03-04 18:02:37 -0800

    3 one-liners!!!

--- assignment3.py
+++ assignment3.py
@@ -20,32 +20,13 @@ def radians_to_degrees(radians):
     return radians * (180 / pi)
 
 def vector_add(*vectors):
-    v1, v2, v3 = [], [], []
-    end1, end2, end3 = 0, 0, 0
-    for i in vectors:
-        v1.append(i[0])
-        v2.append(i[1])
-        v3.append(i[2])
-    for val1, val2, val3 in zip(v1, v2, v3):
-        end1 += val1
-        end2 += val2
-        end3 += val3
-    return [end1, end2, end3]
+    return [sum([i[0] for i in [[val1, val2, val3] for val1, val2, val3 in vectors]]), sum([i[1] for i in [[val1, val2, val3] for val1, val2, val3 in vectors]]), sum([i[2] for i in [[val1, val2, val3] for val1, val2, val3 in vectors]])]
 
 def find_line(a, b):
-    slope = round((b[1] - a[1]) / (b[0] - a[0]), 2)
-    y_int = round(a[1] - slope * a[0], 2)
-    return "y = " + str(slope) + "x + " + str(y_int)
+    return "y = " + str(round((b[1] - a[1]) / (b[0] - a[0]), 2)) + "x + " + str(round(a[1] - round((b[1] - a[1]) / (b[0] - a[0]), 2) * a[0], 2))
 
 def barbell_centroid(left, right):
-    if left > 10 or right > 10:
-        return "Error"
-    x_left, x_right = 0, 0
-    for i in range(0, left):
-        x_left += 45 * ((-2 * i - 1) - 25)
-    for i in range(0, right):
-        x_right += 45 * ((2 * i + 1) + 25)
-    return ((x_left + x_right) / (45 * (left + right)))
+    return "Error" if left > 10 or right > 10 else ((sum([(45 * ((-2 * i - 1) - 25)) for i in range(0, left)]) + sum([(45 * ((2 * i + 1) + 25)) for i in range(0, right)])) / ((left + right) * 45))
 
-print(barbell_centroid(0, 10))
+print(vector_add((2, 1, 3), (1, 1, 1)))
         
