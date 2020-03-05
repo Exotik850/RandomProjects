16bb17f32049b051332383b326fc9181dc8460cf
Exotik850 <kidkool850@gmail.com>
2020-03-04 22:06:20 -0800

    Done with engr project

--- assignment3.py
+++ assignment3.py
@@ -3,21 +3,7 @@ import math
 pi = math.pi
 
 def my_atan(y, x):
-    if x > 0:
-        return math.atan(y/x)
-    elif y >= 0 and x < 0:
-        return math.atan(y/x + pi)
-    elif y < 0 and x < 0:
-        return math.atan(y/x - pi)
-    elif y > 0 and x == 0:
-        return pi/2
-    elif y < 0 and x == 0:
-        return -pi/2
-    else:
-        return 0
-
-def radians_to_degrees(radians):
-    return radians * (180 / pi)
+    return math.atan(y/x) if x > 0 else math.atan(y/x + pi) if y >= 0 and x < 0 else math.atan(y/x - pi) if y < 0 and x < 0 else pi/2 if y > 0 and x == 0 else -pi/2 if y < 0 and x == 0 else 0
 
 def vector_add(*vectors):
     return [sum([i[0] for i in [[val1, val2, val3] for val1, val2, val3 in vectors]]), sum([i[1] for i in [[val1, val2, val3] for val1, val2, val3 in vectors]]), sum([i[2] for i in [[val1, val2, val3] for val1, val2, val3 in vectors]])]
@@ -28,5 +14,7 @@ def find_line(a, b):
 def barbell_centroid(left, right):
     return "Error" if left > 10 or right > 10 else ((sum([(45 * ((-2 * i - 1) - 25)) for i in range(0, left)]) + sum([(45 * ((2 * i + 1) + 25)) for i in range(0, right)])) / ((left + right) * 45))
 
-print(vector_add((2, 1, 3), (1, 1, 1)))
+def cramers_rule(A, b):
+    return "Error" if len(b) != len(A) else [[b[i] * A[i][j] for j in range(len(b))] for i in range(len(A))]
+
         
