3509473b2b8a1bb336a259c3a3fc42569e5668f0
Exotik850 <kidkool850@gmail.com>
2020-03-07 14:25:56 -0800

    hm

--- assignment3.py
+++ assignment3.py
@@ -8,6 +8,7 @@ def my_atan(y, x):
 def vector_add(*vectors):
     return [sum([i[0] for i in [[val1, val2, val3] for val1, val2, val3 in vectors]]), sum([i[1] for i in [[val1, val2, val3] for val1, val2, val3 in vectors]]), sum([i[2] for i in [[val1, val2, val3] for val1, val2, val3 in vectors]])]
 
+
 def find_line(a, b):
     return "y = " + str(round((b[1] - a[1]) / (b[0] - a[0]), 2)) + "x + " + str(round(a[1] - round((b[1] - a[1]) / (b[0] - a[0]), 2) * a[0], 2))
 
@@ -19,5 +20,3 @@ def cramers_rule(A, b):
 
 def is_barbell_stable(left, right):
     return False if barbell_centroid(left, right) < -25 or barbell_centroid(left, right) > 25 else True
-
-        
