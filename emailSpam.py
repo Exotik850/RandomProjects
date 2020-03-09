9a3f10519b6716702f6812c82707457858fc0129
Exotik850 <kidkool850@gmail.com>
2020-03-09 09:49:34 -0700

    optimized spam

--- emailSpam.py
+++ emailSpam.py
@@ -1,4 +1,5 @@
 import yagmail
+from time import sleep
 
 class SpamMail():
     def __init__(self):
@@ -7,12 +8,17 @@ class SpamMail():
         self.yag = yagmail.SMTP(self.email, self.password)
     def Spam(self, count, email):
         for i in range(count):
-            self.yag.send(email, str(i), "Hahaha I've sent you this " + str((i-1)) + " times before!")
-            print("Email number " + str(i) + " sent.")
+            try:
+                self.yag.send(email, str(i), "Hahaha I've sent you this " + str((i-1)) + " times before!")
+            except:
+                print("Error occurred")
+            else:            
+                print("Email number " + str(i) + " sent.")
+            sleep(1)
 
 if __name__ == '__main__':
     SpamMail.__init__(SpamMail)
-    SpamMail.Spam(SpamMail, 15, 'samshiberou@gmail.com' )
+    SpamMail.Spam(SpamMail, 150, 'brindha.kottu@gmail.com' )
 
 
 
