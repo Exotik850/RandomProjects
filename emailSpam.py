c7414f5cce7fcbe761309ae459d3b23ba3f15499
Exotik850 <kidkool850@gmail.com>
2020-03-09 22:10:20 -0700

    pfft

--- emailSpam.py
+++ emailSpam.py
@@ -1,6 +1,8 @@
 import yagmail
+import time
 from time import sleep
 import concurrent.futures
+import threading
 
 class SpamMail:
     def __init__(self):
@@ -11,20 +13,24 @@ class SpamMail:
     def Spam(self, count, email):
         for i in range(int(count)):
             try:
-                with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-                    executor.map(self.yag.send(email, str(i), "Hahaha I've sent you this " + str(i) + " times before!"), email)
+                self.yag.send(email, str(i), "Hahaha I've sent you this " + str(i) + " times before!")
             except:
-                print("Error occurred")
-            else:            
-                print("Email number " + str(i) + " sent.")
+                print("Error Occured")
+            else:
+                print(f"Email #{i + 1} sent")
             sleep(0.1)
-    
-    
+
 if __name__ == '__main__':
     count = input("How many emails: ")
-    email = [input("Email: ")] * 5
+    email = input("Email: ")
+    startTime = time.time()
     sm = SpamMail()
-    sm.Spam(count, email)
+
+    timeTaken = time.time() - startTime
+    print(f"Done in {timeTaken} second(s)")
+    
+        
+    
 
 
 
