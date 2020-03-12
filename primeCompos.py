724019f469727d576ae6a30d1329a8440aa658a6
Exotik850 <kidkool850@gmail.com>
2020-03-12 06:42:46 -0700

    ayyy

--- primeCompos.py
+++ primeCompos.py
@@ -1,42 +1,41 @@
 import math
-
 #n != 2k**2 + p
-
 def isPrime(num):
-    if num == 1 or num == 0 or num == 2:
+    if num == 1 or num == 0:
         return False
-    divisors = []
-    for i in range(2, num):
+    if num == 2:
+        return True
+    for i in range(2, num//2):
         if (num % i) == 0:
             return False
     return True
 
+def isPerfSquare(num):
+    perfSquare = math.sqrt(num / 2)
+    return (perfSquare == (int(perfSquare)))
+
+
 def main():
-    n = 0
-    found = False
+    n = 1
+    found = True
+    primes = list(filter(lambda num: num != 0, [(i * isPrime(i)) for i in range(10000)]))
+        
     while not found:
+        n += 2
+
         if (n % 2) == 0:
-            print(f"It is not {n}")
-            n += 1
+            print (f"It is not {n}")
             continue
 
-        if isPrime(n):
-            print(f"It is not {n}")
-            n += 1
-            continue
-
-        for i in range(1, n):
-            for j in range(1, n):
-                if not isPrime(j):
-                    continue
-                if not ((2 * (i ** 2)) + j == n):
-                    print(f"It is {n}")
-                    found = True
-                    break
-            if found:
+        i = 0
+        found = True
+        while primes[i] <= n:
+            if isPerfSquare(n - primes[i]):
+                print (f"It is not {n}")
+                found = False
                 break
-        n += 1
-                
+            i += 1
+    print(n)
 
 if __name__ == '__main__':
     main()
